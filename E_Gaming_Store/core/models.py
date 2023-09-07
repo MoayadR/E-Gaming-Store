36a06dab8bda992746c54c689b63672d7f3c15d9
MoayadR <moayad.r.abdalraouf@gmail.com>
2023-09-07 17:17:33 +0300

    + Cart UI , Major UI changes , Minor Models Changes , Fixed Bugs

--- E_Gaming_Store/core/models.py
+++ E_Gaming_Store/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import PIL
 
 # Create your models here.
 class Product(models.Model):
@@ -8,35 +9,35 @@ class Product(models.Model):
     region = models.CharField(max_length=100)
     price = models.IntegerField()
     stock = models.IntegerField()
-    image = models.ImageField(null=True, blank=True, upload_to="img/")
+    image = models.ImageField(null=True, blank=True, upload_to="ProductImages/")
 
     def __str__(self):
         return str(self.name)
 
 class Payment(models.Model):
-    user_id = models.ForeignKey( User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey( User, on_delete=models.CASCADE, blank=True, null=True)
     product_id = models.ForeignKey( Product, on_delete=models.CASCADE, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.user_id)
+        return str(self.user)
 
 class Cart(models.Model):
-    user_id = models.ForeignKey( User, on_delete=models.CASCADE, blank=True, null=True)
+    user= models.ForeignKey( User, on_delete=models.CASCADE, blank=True, null=True)
     
     def __str__(self):
-        return str(self.user_id)
+        return str(self.user)
 
 class Cart_Item(models.Model):
-    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE, blank=True, null=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return str(self.cart_id)
+        return str(self.cart)
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return str(self.name)
--- E_Gaming_Store/core/views.py
+++ E_Gaming_Store/core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Cart , Cart_Item
 
 # Create your views here.
 def index(request):
+    if request.user.is_authenticated:
+        userCart = Cart.objects.get(user = request.user)
+        userCartItems = Cart_Item.objects.filter(cart = userCart)
+        numberOfItems = userCartItems.count()
+        return render(request , 'core/index.html' , {"numberOfItems" : numberOfItems , 'userCartItems': userCartItems})
     return render(request , 'core/index.html')
+    
